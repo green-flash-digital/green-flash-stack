a67cf8ec846acbbdcdfa6c5e4be843a6e82c13fb
Drew DeCarme <drewdecarme@gmail.com>
2025-12-18 14:05:56 -0500

    add some style examples

--- packages/stratum-ui-react/src/toast/toast.stories.tsx
+++ packages/stratum-ui-react/src/toast/toast.stories.tsx
@@ -1,11 +1,15 @@
 import { randSentence } from "@ngneat/falso";
 import { css } from "@linaria/core";
 import { classes } from "@stratum-ui/core";
+import type { Meta } from "@storybook/react-vite";
 
 import { Toaster } from "./Toaster.js";
 
-const meta = {
+const meta: Meta = {
   title: "Toasts",
+  parameters: {
+    layout: "centered",
+  },
 };
 export default meta;
 
@@ -41,7 +45,9 @@ const toastStyles = css`
   &.success {
     background-color: green;
   }
+`;
 
+const toastStylesSlideDown = css`
   @keyframes toast-in {
     from {
       opacity: 0;
@@ -55,7 +61,7 @@ const toastStyles = css`
   animation: toast-in 350ms cubic-bezier(0.32, 0.72, 0, 1.12);
 `;
 
-const ToasterWithContainerStyles = new Toaster({
+const ToasterWithTopDown = new Toaster({
   toastDuration: 5,
   containerProps: {
     id: "toaster-strudel",
@@ -79,23 +85,97 @@ const ToasterWithContainerStyles = new Toaster({
     `,
   },
   ToastComponent({ message, variant }) {
-    return <div className={classes(toastStyles, variant)}>{message}</div>;
+    return (
+      <div className={classes(toastStyles, toastStylesSlideDown, variant)}>
+        {message}
+      </div>
+    );
   },
 });
-export function WithContainerStyles() {
+export function WithTopDown() {
   return (
     <>
-      <ToasterWithContainerStyles.Render />
+      <ToasterWithTopDown.Render />
+      <button
+        onClick={() => ToasterWithTopDown.success({ message: randSentence() })}
+      >
+        Success
+      </button>
       <button
         onClick={() =>
-          ToasterWithContainerStyles.success({ message: randSentence() })
+          ToasterWithTopDown.error({
+            message: "error!\n".concat(randSentence()),
+          })
+        }
+      >
+        Error
+      </button>
+    </>
+  );
+}
+
+const stylesToastSlideIn = css`
+  @keyframes toast-slide-in {
+    from {
+      opacity: 0;
+      transform: translateX(30px) scale(0.98);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0) scale(1);
+    }
+  }
+  animation: toast-slide-in 350ms cubic-bezier(0.32, 0.72, 0, 1.12);
+`;
+
+const ToasterWithBottomRightUp = new Toaster({
+  toastDuration: 5,
+  containerProps: {
+    id: "toaster-strudel",
+    className: css`
+      box-sizing: border-box;
+      border: 0;
+      margin: 0;
+      position: fixed;
+      top: unset;
+      left: unset;
+      bottom: 0;
+      right: 0;
+      width: 400px;
+      display: flex;
+      flex-direction: column; // this is important
+      gap: 1rem;
+      padding: 1rem;
+
+      * {
+        box-sizing: border-box;
+      }
+
+      ${stylesToastSlideIn};
+    `,
+  },
+  ToastComponent({ message, variant }) {
+    return (
+      <div className={classes(toastStyles, stylesToastSlideIn, variant)}>
+        {message}
+      </div>
+    );
+  },
+});
+export function WithBottomRight() {
+  return (
+    <>
+      <ToasterWithBottomRightUp.Render />
+      <button
+        onClick={() =>
+          ToasterWithBottomRightUp.success({ message: randSentence() })
         }
       >
         Success
       </button>
       <button
         onClick={() =>
-          ToasterWithContainerStyles.error({
+          ToasterWithBottomRightUp.error({
             message: "error!\n".concat(randSentence()),
           })
         }
